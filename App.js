599620cad5cd6f462375e00ea76f10f3be846ea3
boureimatapily <boureimatapily1@gmail.com>
2020-05-05 14:52:30 +0000

    MOD add Navigator

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+import ArticleDetails from './src/components/ArticleDetails';
+import ArticleListScreen from './src/screens/ArticleListScreen';
 
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <NavigationContainer >
+          <Stack.Navigator initialRouteName="ArticleDetails"
+            screenOptions={{ headerTitleAlign:"center"}}>
+            <Stack.Screen  name="ArticleDetails" component={ArticleDetails} />
+            <Stack.Screen  name="ArticleList" component={ArticleListScreen} />
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
