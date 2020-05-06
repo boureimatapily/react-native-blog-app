f208a358b7c49a16b1ea7257cae7e63e51cfd7d7
boureimatapily <boureimatapily1@gmail.com>
2020-05-06 16:11:10 +0000

    MOD add headrer Style

--- App.js
+++ App.js
@@ -11,9 +11,26 @@ export default function App() {
   return (
       <NavigationContainer >
           <Stack.Navigator initialRouteName="ArticleDetails"
-            screenOptions={{ headerTitleAlign:"center"}}>
-            <Stack.Screen  name="ArticleDetails" component={ArticleDetails} />
-            <Stack.Screen  name="ArticleList" component={ArticleListScreen} />
+             >
+
+            <Stack.Screen  name="ArticleDetails" component={ArticleDetails} 
+                  options={{
+                    headerTitleAlign:"center",
+                  headerLeft:()=>(<Text>Beauty</Text>),
+                  headerRight:()=>(<Text>Beauty</Text>),
+                  headerLeftContainerStyle:{marginLeft:20,
+                               backgroundColor:"violet",
+                               width:160,
+                               
+                                              },
+                  headerRightContainerStyle:{marginRight:20},
+                  headerTitle:null,
+                  headerStyle:{height:120}
+                  }}
+            />
+            <Stack.Screen  name="ArticleList" component={ArticleListScreen} 
+                
+            />
           </Stack.Navigator>
       </NavigationContainer>
   );
