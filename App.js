aba6de8a9c7b16524d7f4e26818d9bf7c0cb3798
boureimatapily <boureimatapily1@gmail.com>
2020-05-06 17:45:53 +0000

    MOD render ArticleItem

--- App.js
+++ App.js
@@ -4,32 +4,34 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack'
 import ArticleDetails from './src/components/ArticleDetails';
 import ArticleListScreen from './src/screens/ArticleListScreen';
+import Icons from './src/components/Icons';
+import { color } from 'react-native-reanimated';
+import ArticleItem from './src/components/ArticleItem';
 
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
       <NavigationContainer >
-          <Stack.Navigator initialRouteName="ArticleDetails"
-             >
+          <Stack.Navigator>
+            <Stack.Screen name="ArticleItem" component={ArticleItem} />
+            <Stack.Screen  name="ArticleList" component={ArticleListScreen}  />
 
             <Stack.Screen  name="ArticleDetails" component={ArticleDetails} 
                   options={{
                     headerTitleAlign:"center",
-                  headerLeft:()=>(<Text>Beauty</Text>),
-                  headerRight:()=>(<Text>Beauty</Text>),
+                  headerLeft:()=>(<Text style={{fontSize:30}, {fontWeight:"bold"}}>Beauty</Text>),
+                  headerRight:()=>(<Icons />),
                   headerLeftContainerStyle:{marginLeft:20,
                                backgroundColor:"violet",
                                width:160,
+                               paddingLeft:10
                                
                                               },
                   headerRightContainerStyle:{marginRight:20},
                   headerTitle:null,
                   headerStyle:{height:120}
                   }}
-            />
-            <Stack.Screen  name="ArticleList" component={ArticleListScreen} 
-                
             />
           </Stack.Navigator>
       </NavigationContainer>
