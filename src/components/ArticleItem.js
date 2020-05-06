4eb5f66a4960e18382c7b4d2cc94b64c3ba5425a
boureimatapily <boureimatapily1@gmail.com>
2020-05-06 17:44:48 +0000

    ADD ArticleItem.js

---
+++ src/components/ArticleItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export class ArticleItem extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+       <Text>hello</Text>
+      </View>
+    )
+  }
+}
+
+const styles= StyleSheet.create({
+
+})
+
+export default ArticleItem
