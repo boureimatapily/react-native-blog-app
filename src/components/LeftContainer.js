3ba18d207dfe9b9b8a5b70dad534626c916dd08c
boureimatapily <boureimatapily1@gmail.com>
2020-05-06 17:45:32 +0000

    MOD style

---
+++ src/components/LeftContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class LeftContainer extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{fontWeight:"bold"},{fontSize:20}}> Beauty </Text>
+      </View>
+    )
+  }
+}
+
