6cf876f84504a07efec48cebacff0cfe3792f844
boureimatapily <boureimatapily1@gmail.com>
2020-05-10 01:17:00 +0000

    MOD move ArticleDetail to screen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack'
-import ArticleDetails from './src/components/ArticleDetails';
+import ArticleDetails from './src/screens/ArticleDetails';
 import ArticleListScreen from './src/screens/ArticleListScreen';
 import Icons from './src/components/Icons';
 import {Ionicons} from '@expo/vector-icons'
